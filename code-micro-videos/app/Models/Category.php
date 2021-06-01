c17f34bf59ea6ed41185d968bdd77b281b71d91b
Ricardo Malikoski <malikoski@gmail.com>
2021-05-31 23:13:18 -0300

    Fase 1 - Category - Genre

---
+++ code-micro-videos/app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use SoftDeletes, Traits\Uuid;
+
+    protected $fillable = ['id', 'name', 'is_active'];
+    protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string'
+    ];
+
+    public $incrementing = false;
+}
